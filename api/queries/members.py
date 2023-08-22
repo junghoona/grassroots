fb8223b07079c92deb15be263e825bb2352c0e93
long guan <long.w.guan@gmail.com>
2023-08-22 00:39:23 +0000

    #27 Added API Endpoint to get a list of all members

--- api/queries/members.py
+++ api/queries/members.py
@@ -13,38 +13,65 @@ class MemberOut(BaseModel):
     community: int
     person: int
 
+class MemberListOut(BaseModel):
+    members: list[MemberOut]
+
 class MemberRepository:
     def create_member(self, member: MemberIn) -> MemberOut:
         with pool.connection() as conn:
             with conn.cursor() as db:
                 db.execute("SELECT EXISTS(SELECT * FROM communities where id=(%s))", [member.community])
+                community_exist = db.fetchone()[0]
+                db.execute("SELECT EXISTS(SELECT * FROM users where id=(%s))", [member.person])
+                user_exist = db.fetchone()[0]
+                # check if both community and user do not exist
+                if not community_exist and not user_exist:
+                    return {"message": "community and user not found"}
                 # check if community exists
-                if db.fetchone()[0]:
-                    db.execute("SELECT EXISTS(SELECT * FROM users where id=(%s))", [member.person])
-                    # check if user exists
-                    if db.fetchone()[0]:
-                        db.execute("SELECT EXISTS(SELECT * FROM members where person=(%s) AND community=(%s))", [member.person, member.community])
-                        # check if member is already in community
-                        if db.fetchone()[0]:
-                            return {"message": "member is already in community"}
-                        result = db.execute(
-                            """
-                            INSERT INTO members (
-                                community,
-                                person
-                            )
-                            VALUES (%s, %s)
-                            RETURNING id;
-                            """,
-                            [
-                                member.community,
-                                member.person
-                            ]
-                        )
-                        id = result.fetchone()[0]
-                        old_data = member.dict()
-                        return MemberOut(id=id, **old_data)
-                    else:
-                        return {"message": "user not found"}
-                else:
+                if not community_exist:
                     return {"message": "community not found"}
+                # check if user exists
+                if not user_exist:
+                    return {"message": "user not found"}
+                db.execute("SELECT EXISTS(SELECT * FROM members where person=(%s) AND community=(%s))", [member.person, member.community])
+                # check if member is already in community
+                if db.fetchone()[0]:
+                    return {"message": "member is already in community"}
+                result = db.execute(
+                    """
+                    INSERT INTO members (
+                        community,
+                        person
+                    )
+                    VALUES (%s, %s)
+                    RETURNING id;
+                    """,
+                    [
+                        member.community,
+                        member.person
+                    ]
+                )
+                id = result.fetchone()[0]
+                old_data = member.dict()
+                return MemberOut(id=id, **old_data)
+
+
+    def get_all_members(self) -> MemberOut:
+        with pool.connection() as conn:
+            with conn.cursor() as db:
+                db.execute(
+                    """
+                    SELECT id, community, person
+                    FROM members
+                    ORDER BY community, person
+                    """
+                )
+                results = []
+                print("db.description", db.description)
+                for row in db.fetchall():
+                    record = {}
+                    for i, column in enumerate(db.description):
+                        record[column.name] = row[i]
+                        print(record)
+                    results.append(MemberOut(**record))
+                return results
--- api/routers/members.py
+++ api/routers/members.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, Response
-from queries.members import MemberIn, MemberOut, MemberRepository, Error
+from queries.members import MemberIn, MemberOut, MemberRepository, Error, MemberListOut
 from typing import Union
 
 router = APIRouter()
@@ -10,13 +10,31 @@ def create_member(
     response: Response,
     repo: MemberRepository = Depends(),
 ):
+    """
+    Create a new member with the following info:
+    - community: existing community.id
+    - person: existing user.id
+    """
     result = repo.create_member(member)
     try: # return error message
-        if result.get("message") == "user not found" or result.get("message") == "community not found":
-            response.status_code = 404
-            return result
-        elif result.get("message") == "member is already in community":
+        if result.get("message") == "member is already in community":
             response.status_code = 400
+            return result
+        else:
+            # for "user not found" , "community not found", or "user and communit not found"
+            response.status_code = 404
             return result
     except: # return created member
         return result
+
+@router.get("/api/members/", response_model=MemberListOut)
+def get_members(
+    repo: MemberRepository = Depends(),
+):
+    """
+    Get all members and returns a list with the following:
+    - id: members.id
+    - community: communities.id
+    - person: users.id
+    """
+    return {"members": repo.get_all_members()}
