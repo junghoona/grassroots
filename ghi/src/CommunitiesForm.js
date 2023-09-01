559348703b48a5e48e821a8ff10d700dccb48aac
John An <junghooonan@gmail.com>
2023-09-01 00:47:49 +0000

    #50 added Communities form

--- ghi/src/CommunitiesForm.js
+++ ghi/src/CommunitiesForm.js
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react";
 
-function CommunitiesForm() {
+const CommunitiesForm = () => {
   // useState hooks to define input fields
   const [name, setName] = useState("");
-  const [city, setCity] = useState([]);
+  const [city, setCity] = useState("");
   const [states, setStates] = useState([]);
+  const [state, setState] = useState("");
   const [type, setType] = useState("");
   const [description, setDescription] = useState("");
   const [creatorID, setCreatorID] = useState("");
@@ -21,14 +22,13 @@ function CommunitiesForm() {
       const url = `https://parseapi.back4app.com/classes/Usabystate_States?limit=50&order=name&keys=name,objectId,population,postalAbreviation&where=${where}`;
       const response = await fetch(url, {
         headers: {
-          "X-Parse-Application-Id": "YeFK5eZAEn05owCNmcWhucKigaBpM00alBP4QdCX", // This is your app's application id
-          "X-Parse-REST-API-Key": "YkBFQLNkHV3fojvuhqCsfTBCjTAjU4xfFYaUaPQd", // This is your app's REST API key
+          "X-Parse-Application-Id": "YeFK5eZAEn05owCNmcWhucKigaBpM00alBP4QdCX",
+          "X-Parse-REST-API-Key": "YkBFQLNkHV3fojvuhqCsfTBCjTAjU4xfFYaUaPQd",
         },
       });
 
       if (response.ok) {
-        const data = await response.json(); // Here you have the data that you need
-        console.log("DATA: ", JSON.stringify(data, null, 2));
+        const data = await response.json();
         setStates(data["results"]);
       }
     }
@@ -37,37 +37,39 @@ function CommunitiesForm() {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    // create an empty data obj
-    const data = {};
 
-    // assign values to the key names that backend server is expecting
-    data.name = name;
-    data.city = city;
-    data.state = states;
-    data.type = type;
-    data.description = description;
-    data.creator_id = creatorID;
-
-    const url = `${process.env.REACT_APP_API_HOST}/api/communities`;
-    const fetchConfig = {
-      method: "post",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json",
-      },
+    const data = {
+      name: name,
+      city: city,
+      state: state,
+      type: type,
+      description: description,
+      creator_id: creatorID,
     };
 
-    const response = await fetch(url, fetchConfig);
+    const response = await fetch(
+      `${process.env.REACT_APP_API_HOST}/api/communities`,
+      {
+        method: "post",
+        credentials: "include",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
     if (response.ok) {
       const newCommunity = await response.json();
-      console.log(newCommunity);
 
       setName("");
       setCity("");
-      setStates("");
+      setState("");
       setType("");
       setDescription("");
       setCreatorID("");
+    } else {
+      console.error(response);
     }
   };
 
@@ -103,22 +105,28 @@ function CommunitiesForm() {
               />
               <label htmlFor="last_name">City</label>
             </div>
+            <label htmlFor="state" className="form-label">
+              Choose your state
+            </label>
             <div className="form-floating mb-3">
-              <label htmlFor="state" className="form-label">
-                Choose your state
-              </label>
               <select
+                onChange={(e) => setState(e.target.value)}
                 required
                 className="form-select"
-                id="states"
+                id="state"
                 aria-label="Choose your state"
               >
                 <option value="">Open this select menu</option>
-                {states.map((state) => (
-                  <option key={state.objectId} value={state.objectId}>
-                    {state.name}
-                  </option>
-                ))}
+                {states.map((state) => {
+                  return (
+                    <option
+                      key={state.objectId}
+                      value={state.postalAbbreviation}
+                    >
+                      {state.name}
+                    </option>
+                  );
+                })}
               </select>
             </div>
             <div className="form-floating mb-3">
@@ -166,6 +174,6 @@ function CommunitiesForm() {
       </div>
     </div>
   );
-}
+};
 
 export default CommunitiesForm;
