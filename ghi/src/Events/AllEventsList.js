6ab36064715b1baa179f81f46ff8dd29606bc563
Parsa Abdizadeh <parsaa.0110@gmail.com>
2023-09-09 23:24:26 +0000

    #68 Filter events by state

--- ghi/src/Events/AllEventsList.js
+++ ghi/src/Events/AllEventsList.js
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import useToken from "@galvanize-inc/jwtdown-for-react";
 
 const AllEventList = () => {
   const [events, setEvents] = useState([]);
   const [states, setStates] = useState([]);
   const [selectedState, setSelectedState] = useState("");
+  const { token } = useToken();
 
   async function getAllEventsData() {
     const url = `${process.env.REACT_APP_API_HOST}/api/events`;
@@ -50,86 +52,158 @@ const AllEventList = () => {
   useEffect(() => {
     getAllEventsData();
     fetchStatesAPI();
-  }, []);
+  }, [token]);
 
   return (
-    <div className="container text-center">
-      <header className="d-flex justify-content-between align-items-center p-2">
-        <h1 className="mt-5">Upcoming Events</h1>
-        <div className="input-group mt-4 p-2">
-          <select
-            className="form-select"
-            onChange={(e) => setSelectedState(e.target.value)}
-            value={selectedState}
-          >
-            <option value="">All States</option>
-            {states.map((state) => (
-              <option key={state.objectId} value={state.name.toLowerCase()}>
-                {state.name}
-              </option>
-            ))}
-          </select>
-        </div>
-        <Link
-          to={`/events/create`}
-          className="btn btn-success btn-block mt-4 p-2 ml-auto"
-          style={{ width: "150px" }}
-        >
-          Create Event
-        </Link>
-      </header>
-      <main>
-        <div className="row justify-content-center">
-          {events
-            .filter((item) => {
-              return (
-                selectedState.toLowerCase() === "" ||
-                item.event.state
-                  .toLowerCase()
-                  .includes(selectedState.toLowerCase())
-              );
-            })
-            .map((event) => (
-              <div
-                className="card mb-4 shadow-lg bg-light"
-                style={{ maxWidth: "800px" }}
-                key={event.id}
+    <div>
+      {token ? (
+        <div className="container text-center">
+          <header className="d-flex justify-content-between align-items-center p-2">
+            <h1 className="mt-5">Upcoming Events</h1>
+            <div className="input-group mt-4 p-2">
+              <select
+                className="form-select"
+                onChange={(e) => setSelectedState(e.target.value)}
+                value={selectedState}
               >
-                <div className="row g-0">
-                  <div className="col-md-4">
-                    <img
-                      src={event.image}
-                      className="img-fluid rounded-start"
-                      alt="Event"
-                      style={{ height: "100%", objectFit: "cover" }}
-                    />
-                  </div>
-                  <div className="col-md-8">
-                    <div className="card-body">
-                      <h5 className="card-title">{event.name}</h5>
-                      <p className="card-text">{event.description}</p>
-                      <p className="card-text">{event.state}</p>
-                      <p className="card-text">
-                        {event.day} from {event.start_time} to {event.end_time}
-                      </p>
-                      <p className="card-text">
-                        <small className="text-muted">
-                          Creator: {event.creator}
-                        </small>
-                      </p>
-                      <Link
-                        to={`/events/${event.id}`}
-                        className="btn btn-primary"
-                      >
-                        View Details
-                      </Link>
+                <option value="">All States</option>
+                {states.map((state) => (
+                  <option key={state.objectId} value={state.name}>
+                    {state.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <Link
+              to={`/events/create`}
+              className="btn btn-success btn-block mt-4 p-2 ml-auto"
+              style={{ width: "150px" }}
+            >
+              Create Event
+            </Link>
+          </header>
+          <main>
+            <div className="row justify-content-center">
+              {events
+                .filter((event) => {
+                  return (
+                    selectedState === "" || event.state.includes(selectedState)
+                  );
+                })
+                .map((event) => (
+                  <div
+                    className="card mb-4 shadow-lg bg-light"
+                    style={{ maxWidth: "800px" }}
+                    key={event.id}
+                  >
+                    <div className="row g-0">
+                      <div className="col-md-4">
+                        <img
+                          src={event.image}
+                          className="img-fluid rounded-start"
+                          alt="Event"
+                          style={{ height: "100%", objectFit: "cover" }}
+                        />
+                      </div>
+                      <div className="col-md-8">
+                        <div className="card-body">
+                          <h5 className="card-title">{event.name}</h5>
+                          <p className="card-text">{event.description}</p>
+                          <p className="card-text">{event.state}</p>
+                          <p className="card-text">
+                            {event.day} from {event.start_time} to{" "}
+                            {event.end_time}
+                          </p>
+                          <p className="card-text">
+                            <small className="text-muted">
+                              Creator: {event.creator}
+                            </small>
+                          </p>
+                          <Link
+                            to={`/events/${event.id}`}
+                            className="btn btn-primary"
+                          >
+                            View Details
+                          </Link>
+                        </div>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </div>
-            ))}
+                ))}
+            </div>
+          </main>
         </div>
-      </main>
+      ) : (
+        <div className="container text-center">
+          <header className="d-flex justify-content-between align-items-center p-2">
+            <h1 className="mt-5">Upcoming Events</h1>
+            <div className="input-group mt-4 p-2">
+              <select
+                className="form-select"
+                onChange={(e) => setSelectedState(e.target.value)}
+                value={selectedState}
+              >
+                <option value="">All States</option>
+                {states.map((state) => (
+                  <option key={state.objectId} value={state.name}>
+                    {state.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </header>
+          <main>
+            <div className="row justify-content-center">
+              {events
+                .filter((event) => {
+                  return (
+                    selectedState === "" || event.state.includes(selectedState)
+                  );
+                })
+                .map((event) => (
+                  <div
+                    className="card mb-4 shadow-lg bg-light"
+                    style={{ maxWidth: "800px" }}
+                    key={event.id}
+                  >
+                    <div className="row g-0">
+                      <div className="col-md-4">
+                        <img
+                          src={event.image}
+                          className="img-fluid rounded-start"
+                          alt="Event"
+                          style={{ height: "100%", objectFit: "cover" }}
+                        />
+                      </div>
+                      <div className="col-md-8">
+                        <div className="card-body">
+                          <h5 className="card-title">{event.name}</h5>
+                          <p className="card-text">{event.description}</p>
+                          <p className="card-text">{event.state}</p>
+                          <p className="card-text">
+                            {event.day} from {event.start_time} to{" "}
+                            {event.end_time}
+                          </p>
+                          <p className="card-text">
+                            <small className="text-muted">
+                              Creator: {event.creator}
+                            </small>
+                          </p>
+                          <Link
+                            to={`/events/${event.id}`}
+                            className="btn btn-primary"
+                          >
+                            View Details
+                          </Link>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+            </div>
+          </main>
+        </div>
+      )}
     </div>
   );
 };
