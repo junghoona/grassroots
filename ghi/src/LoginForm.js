376f9c996263a6f8f474c77dcc123a2936bc0245
John An <junghooonan@gmail.com>
2023-09-14 00:06:15 -0700

    added Tailwind CSS to login/signup form

--- ghi/src/LoginForm.js
+++ ghi/src/LoginForm.js
@@ -16,30 +16,61 @@ const LoginForm = () => {
   };
 
   return (
-    <div className="card text-bg-light mb-3">
-      <h5 className="card-header">Login</h5>
-      <div className="card-body">
-        <form onSubmit={(e) => handleSubmit(e)}>
-          <div className="mb-3">
-            <label className="form-label">Username:</label>
+    <div className="flex flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img
+          className="mx-auto h-10 w-auto"
+          src="https://emoji.slack-edge.com/T03AAE15UA0/cool_beans/a6d470806ddcbd77.png"
+          alt="Green Bean Logo"
+        />
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Sign in
+        </h2>
+      </div>
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" onSubmit={(e) => handleSubmit(e)}>
+          <div>
+            <label
+              htmlFor="username"
+              class="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Username:
+            </label>
             <input
+              required
               name="username"
               type="text"
-              className="form-control"
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </div>
-          <div className="mb-3">
-            <label className="form-label">Password:</label>
-            <input
-              name="password"
-              type="password"
-              className="form-control"
-              onChange={(e) => setPassword(e.target.value)}
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
             />
           </div>
           <div>
-            <input className="btn btn-primary" type="submit" value="Login" />
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Password:
+            </label>
+            <input
+              required
+              name="password"
+              type="password"
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+          </div>
+          <div>
+            <button
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              type="submit"
+              value="Login"
+            >
+              Login
+            </button>
           </div>
         </form>
       </div>
--- ghi/src/SignupForm.js
+++ ghi/src/SignupForm.js
@@ -33,106 +33,163 @@ const SignupForm = () => {
   };
 
   return (
-    <div className="card text-bg-light mb-3">
-      <h5 className="card-header">Register for a new Account</h5>
-      <div className="card-body">
-        <form onSubmit={(e) => handleRegistration(e)}>
-          <div className="mb-3">
-            <label className="form-label">First Name: </label>
+    <div className="flex flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img
+          className="mx-auto h-10 w-auto"
+          src="https://emoji.slack-edge.com/T03AAE15UA0/cool_beans/a6d470806ddcbd77.png"
+          alt="Green Bean Logo"
+        />
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Register for a new Account
+        </h2>
+      </div>
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" onSubmit={(e) => handleRegistration(e)}>
+          <div>
+            <label
+              htmlFor="firstname"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              First Name:
+            </label>
             <input
               required
               name="firstName"
               type="text"
-              className="form-control"
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
               onChange={(e) => {
                 setFirstName(e.target.value);
               }}
             />
           </div>
-          <div className="mb-3">
-            <label className="form-label">Last Name: </label>
+          <div>
+            <label
+              htmlFor="lastName"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Last Name:
+            </label>
             <input
               required
               name="lastName"
               type="text"
-              className="form-control"
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
               onChange={(e) => {
                 setLastName(e.target.value);
               }}
             />
           </div>
-          <div className="mb-3">
-            <label className="form-label">Avatar: </label>
+          <div>
+            <label
+              htmlFor="avatar"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Avatar:
+            </label>
             <input
+              required
               name="avatar"
               type="text"
-              className="form-control"
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               onChange={(e) => {
                 setAvatar(e.target.value);
               }}
             />
           </div>
-          <div className="mb-3">
-            <label className="form-label">Bio: </label>
+          <div>
+            <label
+              htmlFor="bio"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Bio:
+            </label>
             <input
+              required
               name="bio"
               type="text"
-              className="form-control"
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               onChange={(e) => {
                 setBio(e.target.value);
               }}
             />
           </div>
-          <div className="mb-3">
-            <label className="form-label">City: </label>
+          <div>
+            <label
+              htmlFor="city"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              City:
+            </label>
             <input
               required
               name="city"
               type="text"
-              className="form-control"
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               onChange={(e) => {
                 setCity(e.target.value);
               }}
             />
           </div>
-          <div className="mb-3">
-            <label className="form-label">State: </label>
+          <div>
+            <label
+              htmlFor="state"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              State:
+            </label>
             <input
               required
               name="state"
               type="text"
-              className="form-control"
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               onChange={(e) => {
                 setState(e.target.value);
               }}
             />
           </div>
-          <div className="mb-3">
-            <label className="form-label">E-mail: </label>
+          <div>
+            <label
+              htmlFor="username"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              E-mail:
+            </label>
             <input
               required
               name="username"
               type="text"
-              className="form-control"
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               onChange={(e) => {
                 setEmail(e.target.value);
               }}
             />
           </div>
-          <div className="mb-3">
-            <label className="form-label">password</label>
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Password:
+            </label>
             <input
               required
               name="password"
               type="password"
-              className="form-control"
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               onChange={(e) => {
                 setPassword(e.target.value);
               }}
             />
           </div>
           <div>
-            <input className="btn btn-primary" type="submit" value="Register" />
+            <button
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              type="submit"
+              value="Register"
+            >
+              Create new account
+            </button>
           </div>
         </form>
       </div>
